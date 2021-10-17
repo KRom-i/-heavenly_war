d75e609b8a607931019305aee96bf31d193e8123
KRom-i <emailroma@mail.ru>
2021-10-18 02:49:35 +0300

    Home work

--- core/src/stargame/gb/ru/StarGame.java
+++ core/src/stargame/gb/ru/StarGame.java
@@ -1,33 +1,40 @@
 package stargame.gb.ru;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import java.util.Arrays;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture backgroundImg;
+	int backgroundWidth, backgroundHeight;
+	final float BACK_X = 0;
+	final float BACK_Y = 0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		backgroundImg = new Texture("background_img.jpg");
+		backgroundWidth = Gdx.graphics.getWidth();
+		backgroundHeight = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0,0);
+		batch.setColor(Color.BLACK);
+		batch.draw(backgroundImg, BACK_X, BACK_Y, backgroundWidth, backgroundHeight);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		backgroundImg.dispose();
 	}
 }
